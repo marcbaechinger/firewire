95957e2d2cc2876f54a141f777266cd6e86ccec4
marcbaechinger <marc.baechinger@gmail.com>
2013-06-18 21:15:34 +0200

    make directives to switch carousel slide on user action.

--- public/resultapp/app/namespaces.js
+++ public/resultapp/app/namespaces.js
@@ -1,1 +1,17 @@
 var ResultApp = angular.module('ResultApp', ['ngResource', 'common.services']);
+
+ResultApp
+	.directive('carouselPrev', [function() {
+	  return function($scope, elm, attrs) {
+		  elm.on("click", function () {
+			  $(attrs.carouselPrev).carousel("prev");
+		  });
+	  };
+	}])
+	.directive('carouselNext', [function() {
+	  return function($scope, elm, attrs) {
+		  elm.on("click", function () {
+			  $(attrs.carouselNext).carousel("next");
+		  });
+	  };
+	}]);
