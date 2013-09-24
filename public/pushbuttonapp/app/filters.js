d1591e6a3ffbf88273a7ef80394399b97712908e
marcbaechinger <marc.baechinger@gmail.com>
2013-09-24 22:40:27 +0200

    completed image banner of pushbutton app

--- public/pushbuttonapp/app/filters.js
+++ public/pushbuttonapp/app/filters.js
@@ -2,4 +2,18 @@
 
 /* Filters */
 
-angular.module('pushbutton.filters', []);
+angular.module('pushbutton.filters', [])
+	.filter('seconds2time', function() {
+    return function(input, uppercase) {
+		var seconds, 
+			timestring = "" + parseInt(input/60, 10);
+		
+		timestring += ":";
+		seconds = input % 60;
+		if (seconds <= 9) {
+			timestring += "0";
+		}
+		timestring += seconds;
+		return timestring;
+    }
+  });;
